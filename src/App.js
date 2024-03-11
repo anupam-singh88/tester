748016e5248c6559b1d388df4acfc504d1ca9a40
anupam singh <anupamsingh2389@gmail.com>
2024-03-11 16:20:51 +0530

    changing script

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link, Route, Routes } from 'react-router-dom'
 import Success from './components/Success'
 import New from './components/New'
 
 const App = () => {
+  useEffect(() => {
+    console.log(window.trackdesk)
+    if (window.trackdesk) {
+      console.log('initiating trackdesk...')
+      window.trackdesk("lennyai", "click");
+    }
+
+  }, [])
   return (
     <Routes>
       {/* <div> */}
